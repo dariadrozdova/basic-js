21c8e7746bc7a34f3f76d2594b52dd85649289d4
Daria Drozdova <dariadrozdova10@gmail.com>
2020-11-01 22:44:28 +0300

    feat: add an implementation of the transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,36 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if (!arr || !(arr instanceof Array)) {
+        throw Error;
+    }
+    const transformed = [];
+    const discardedIndex= new Set();
+    for (let i = 0; i < arr.length; i++) {
+        switch (arr[i]) {
+            case '--discard-next':
+                i++;
+                discardedIndex.add(i);
+                break;
+            case '--discard-prev':
+                if (i > 0 && !discardedIndex.has(i - 1)) {
+                    discardedIndex.add(i - 1);
+                    transformed.pop();
+                }
+                break;
+            case '--double-next':
+                if (i < arr.length - 1) {
+                    transformed.push(arr[i + 1]);
+                }
+                break;
+            case '--double-prev':
+                if (i > 0 && !discardedIndex.has(i - 1)) {
+                    transformed.push(arr[i - 1]);
+                }
+                break;
+            default:
+                transformed.push(arr[i]);
+        }
+    }
+    return transformed;
 };
