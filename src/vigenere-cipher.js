64960a12bfd7d46806ad76b94de9d99d47d6e9bd
Daria Drozdova <dariadrozdova10@gmail.com>
2020-11-01 23:37:16 +0300

    feat: add an implementation of the vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,53 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(type = true) {
+    this.type = type ? 'direct' : 'reverse';
+  }
+
+  encrypt(message, key) {
+
+    if (!message || !key) throw new Error();
+
+    const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let result = '';
+
+    for (let i = 0, j = 0; i < message.length; i++) {
+      if (message[i].match(/[A-Z]/)) {
+        let charNum = (message[i].charCodeAt() + (key[j % key.length]).charCodeAt() - 130) % 26;
+        let char = alphabet.charAt(charNum);
+        result += char;
+        j++;
+      } else {
+        result += message[i];
+      }
+    }
+
+    return this.type === 'reverse' ? result.split('').reverse().join('') : result;
+  }
+
+
+  decrypt(message, key) {
+    if (!message || !key) throw new Error();
+
+    const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let result = '';
+
+    for (let i = 0, j = 0; i < message.length; i++) {
+      if (message[i].match(/[A-Z]/)) {
+        let charNum = (message[i].charCodeAt() - (key[j % key.length]).charCodeAt() + 104) % 26;
+        let char = alphabet.charAt(charNum);
+        result += char;
+        j++;
+      } else {
+        result += message[i];
+      }
+    }
+    return this.type === 'reverse' ? result.split('').reverse().join(''): result;
   }
 }
 
